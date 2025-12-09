c61249819a7a7f2405fe0a9c6a112de6d40f289a
sirlencio <carlos.l.p.251001@gmail.com>
2025-12-09 18:49:53 +0100

    fixed images

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>@yield('title')</title>
+    <link rel="icon" href="{{ asset('favicon.ico') }}">
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <!-- Styles -->
